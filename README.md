103dffc5de58ffa562aaed2ac99cb462d38bed23
Burney <38512280+h4ppyhacker@users.noreply.github.com>
2021-02-09 01:57:18 -0600

    Challenge 1
    Completed challenge 1.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# Burney-Daily-Programmer
+# Burney Daily Programmer
  This is my repository for the challenges listed on r/dailyprogrammer.
+
+Source: https://www.reddit.com/r/dailyprogrammer/wiki/challenges
+
+Programs:
+1) Create a program that will ask the user's name, age and reddit username. Have it tell the information back.
--- src/App.java
+++ src/App.java
@@ -1,5 +1,28 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import java.util.Scanner; // Import Scanner class so user input may be used.
+
+class App{
+    public static void Day1(){
+        // Declare variables for name, age, and reddit username.
+            
+            // Ask user for their name.
+            System.out.print("What is your name? ");
+            Scanner scan = new Scanner(System.in);
+            String realName = scan.nextLine();
+            
+            // Ask user for their age.
+            System.out.print("How old are you? ");
+            int userAge = scan.nextInt();
+            
+            // Ask user for their reddit username.
+            Scanner scan1 = new Scanner(System.in);
+            System.out.print("What is your reddit username? ");
+            String userName = scan1.nextLine();
+            
+            // Tell user their name, age, and username. Tell them they are fantastic.
+            System.out.println("Your name is " + realName + ", you are " + userAge + " years old, your reddit username is " + userName + ", and you are a pretty fantastic person!");
+    }
+    public static void main(String[] args){
+        // Challenge 1) Ask a user for their name, age, and reddit username. Then print it back out to them!
+        Day1();
     }
 }
